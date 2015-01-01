f5345ac3e0fef517b731f72eb89a8d0dd2a55694
Blake Kelley <blakekelley127@gmail.com>
2015-01-01 14:44:27 -0700

    Eliminate repetition

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,25 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+    @base_title = "Insiduous Designs"
+  end
+  
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Insiduous Designs"
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Insiduous Designs"
+    assert_select "title", "Help | #{@base_title}"
   end
 
 test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Insiduous Designs"
+    assert_select "title", "About | #{@base_title}"
   end
 end
