572ac939212427bc62e95126e22622f3ff44586b
Blake Kelley <blakekelley127@gmail.com>
2015-01-03 12:49:08 -0700

    Created Signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
-
   root             'static_pages#home'
 
   get 'help'    => 'static_pages#help'
@@ -10,6 +8,8 @@ Rails.application.routes.draw do
 
   get 'signup'  => 'users#new'
 
+  get 'users/new'
+  
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
