ea24574ace9b15c2f78f01a7a9971ea0266329f6
Blake Kelley <blakekelley127@gmail.com>
2015-01-04 17:09:11 -0700

    Updated tests for user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  test "invalid signup information" do
+  test "invalid signup: name, email, password, password_confirmation" do
     get signup_path
     assert_no_difference 'User.count' do
       post users_path, user: { name:  "",
@@ -10,6 +10,91 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    assert_select "div#error_explanation ul>li", {:count=>4}
+  end
+
+  test "invalid signup: email, password, password_confirmation" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, user: { name:  "Invalid User",
+                               email: "user@invalid",
+                               password:              "foo",
+                               password_confirmation: "bar" }
+    end
+    assert_template 'users/new'
+    assert_select "div#error_explanation ul>li", {:count=>3}
+  end
+
+  test "invalid signup: name, password, password_confirmation" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, user: { name:  "",
+                               email: "user@invalid.net",
+                               password:              "foo",
+                               password_confirmation: "bar" }
+    end
+    assert_template 'users/new'
+    assert_select "div#error_explanation ul>li", {:count=>3}
+  end
+
+  test "invalid signup: name, email, password_confirmation" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, user: { name:  "",
+                               email: "user@invalid",
+                               password:              "foobar",
+                               password_confirmation: "foo" }
+    end
+    assert_template 'users/new'
+    assert_select "div#error_explanation ul>li", {:count=>3}
+  end
+
+  test "invalid signup: name, email" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, user: { name:  "",
+                               email: "user@invalid",
+                               password:              "foobar",
+                               password_confirmation: "foobar" }
+    end
+    assert_template 'users/new'
+    assert_select "div#error_explanation ul>li", {:count=>2}
+  end
+
+  test "invalid signup: name" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, user: { name:  "",
+                               email: "user@invalid.net",
+                               password:              "foobar",
+                               password_confirmation: "foobar" }
+    end
+    assert_template 'users/new'
+    assert_select "div#error_explanation ul>li", {:count=>1}
+  end
+
+  test "invalid signup: email" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, user: { name:  "Invalid User",
+                               email: "user@invalid",
+                               password:              "foobar",
+                               password_confirmation: "foobar" }
+    end
+    assert_template 'users/new'
+    assert_select "div#error_explanation ul>li", {:count=>1}
+  end
+
+  test "invalid signup: password, password_confirmation" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, user: { name:  "Invalid User",
+                               email: "user@invalid",
+                               password:              "foobar",
+                               password_confirmation: "foobar" }
+    end
+    assert_template 'users/new'
+    assert_select "div#error_explanation ul>li", {:count=>1}
   end
 
   test "valid signup information" do
